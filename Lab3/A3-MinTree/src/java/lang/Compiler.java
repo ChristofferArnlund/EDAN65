345847c98bb6cad9df1f3fcfc1887d172aa19415
Magic.Marcus <marcusrodan@gmail.com>
2016-09-22 12:50:30 +0200

    Sista

--- Lab3/A3-MinTree/src/java/lang/Compiler.java
+++ Lab3/A3-MinTree/src/java/lang/Compiler.java
@@ -10,7 +10,7 @@ public class Compiler {
 	public static void main(String args[]) {
 		Leaf l1 = new Leaf(1);
 		Leaf l2 = new Leaf(2);
-		Leaf l3 = new Leaf(3);
+		Leaf l3 = new Leaf(1);
 		Pair p1 = new Pair(l2, l3);
 		Pair p2 = new Pair(l1, p1);
 		Program p = new Program(p2);
